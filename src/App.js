f71f999e5d7e6683e8b21e3d1313c663c4d1183f
chayan0504 <68233267+chayan0504@users.noreply.github.com>
2022-07-06 22:40:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router,Route, Routes } from 'react-router-dom'
 import FeedbackList from "./components/FeedbackList"
-import FeedbackData from './data/FeedbackData'
+// import FeedbackData from './data/FeedbackData'
 import Header from './components/Header'
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
 import AboutPage from './pages/AboutPage'
 import AboutIconLink from './components/AboutIconLink'
-import Post from './pages/Post'
+// import Post from './pages/Post'
 import { FeedbackProvider } from './context/FeedbackContext'
 
 
